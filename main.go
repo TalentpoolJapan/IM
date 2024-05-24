04c964cc32acc59cb042304a573ca8c02d244780
rogger <zrj@DESKTOP-MK2V6IS>
2024-05-24 13:34:11 +0800

    提交代码

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"imserver/config"
 	"imserver/controllers"
 	"imserver/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -29,12 +30,31 @@ var (
 	}
 )
 
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,TalentPool-Language,token")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		c.Next()
+	}
+}
+
 func main() {
 	//初始化全局内存数据结构
 	initializeData()
 
 	//WS服务开始
 	r := gin.New()
+	r.Use(Cors())
 	r.Use(gin.Recovery())
 	r.GET("/ws", ct.WS)
 	//r.GET("/wst", ct.MockWs)
