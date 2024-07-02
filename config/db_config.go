2fcbc9a262afde82a931a7dd6622250dfb0fb6a1
holger <694133558@qq.com>
2024-07-03 00:07:55 +0800

    配置更新

--- config/db_config.go
+++ config/db_config.go
@@ -10,6 +10,6 @@ import (
 var (
 	MemDB          = gmap.New(true)
 	MysqlDB, _     = xorm.NewEngine("mysql", fmt.Sprintf("root:%s@%s/%s?charset=utf8", MYSQL_SECRECT, MYSQL_HOST, MYSQL_DB))
-	ManticoreDB, _ = xorm.NewEngine("mysql", fmt.Sprintf("``:``@tcp(%s)/Manticore", "13.231.174.2:9306"))
+	ManticoreDB, _ = xorm.NewEngine("mysql", fmt.Sprintf("``:``@tcp(%s)/Manticore", "127.0.0.1:9306"))
 	NoSqlDB, _     = models.NewNoSqlDB("IMKVDB")
 )
